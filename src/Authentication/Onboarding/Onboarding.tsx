757ca5580400bf6fe0f792e37357095cc54b533a
Rafiou Sitou <rafiousitou90@gmail.com>
2020-07-31 17:11:37 -0300

    feat(): Creation of the Boilerplate, adding LoadAssets and fonts.

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+
+// interface OnboardingProps {}
+
+const Onboarding  = () => {
+
+  return (
+      <View style={{ flex: 1, backgroundColor: "cyan" }} />
+  );
+};
+
+export default Onboarding;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as LoadAssets } from './LoadAssets'
